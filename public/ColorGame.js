19677a8b28d1a94b37e20e7beffa16d19098b9e3
Sattwik18 <sattwik18@gmail.com>
2020-06-23 01:38:38 +0530

    third commit

--- public/ColorGame.js
+++ public/ColorGame.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV==="production")
+{
+	app.use(express.static("build"));
+	app.get("*",(req, res) =>{
+		res.sendFile(path.resolve(__dirname,"build", "ColorGame.html"));
+	});
+}
+
 var numSquares=6;
 var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
