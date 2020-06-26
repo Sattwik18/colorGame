eb211db4de6413f265a0b0c8fe94e865980fbb58
Sattwik18 <sattwik18@gmail.com>
2020-06-27 01:58:23 +0530

    fifth commit

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ var app = express();
 
 app.use(express.static("public"))
 
+app.get('/', function(req, res){
+    res.send(colorGame.html)
+})
+
 app.listen(process.env.PORT || 3000, () => console.log("Server is running......"));
